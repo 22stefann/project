a715ae61edfcaaa8e46e635535f63bb816b0a27b
Stefan Stanisavljevic <stefanstanisavljevic777@gmail.com>
2023-11-04 17:48:15 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include("db.php");
+
+// Upit za izvlačenje podataka
+$query = "SELECT * FROM proizvodi";
+$result = $connection->query($query);
+
+if ($result->num_rows > 0) {
+    echo "<h1>Proizvodi:</h1>";
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . " - Naziv: " . $row["name"] . "<br>";
+    }
+} else {
+    echo "Nema rezultata.";
+}
+
+$connection->close();
+?>
